25e6638d6eeb500371877d73d77599e05b074fe0
Aaron Thomas <a.thomas@mixd.co.uk>
2014-03-01 15:06:07 +0000

    added some basic defaults for testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,26 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+############################################
+# Setup Git
+############################################
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, "APPLICATION NAME HERE"
+set :repository, "git@github.com:Mixd/GITHUB-REPO-HERE.git"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules", "capfile", "config/"]
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+############################################
+# Setup Server
+############################################
 
-# Default value for :scm is :git
-# set :scm, :git
+set :use_sudo, false
+ssh_options[:forward_agent] = true
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+############################################
+# Default tasks
+############################################
 
 namespace :deploy do
 
