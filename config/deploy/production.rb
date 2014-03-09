8d74cb3d498cdfacf43fd13318ffc6db7c6cb3a7
Aaron Thomas <a.thomas@mixd.co.uk>
2014-03-09 15:59:14 +0000

    added stage url var to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 ############################################
 
 set :stage, :production
-
+set :stage_url, "http://www.example.com"
 server "XXX.XXX.XX.XXX", user: "SSHUSER", roles: %w{web app db}
 set :deploy_to, "/deploy/to/path"
 
