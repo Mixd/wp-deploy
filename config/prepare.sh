60dccb3206210e4110378c7946e3328f5df17f9a
Dave Naylor <dave@caramboo.com>
2014-09-18 14:36:19 +0100

    Change WordPress branch to 4.0-branch

--- config/prepare.sh
+++ config/prepare.sh
@@ -1,7 +1,7 @@
 rm -rf .git
 git init
 rm -rf wordpress
-git submodule add -b 3.9-branch https://github.com/WordPress/WordPress.git wordpress
+git submodule add -b 4.0-branch https://github.com/WordPress/WordPress.git wordpress
 git remote rm origin
 git add -A
 git commit -m "Inital commit"
