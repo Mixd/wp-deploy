d3d6c25fd45d7f3e4fd2f51730bff90b272a733c
DrizzlyOwl <ashleydavies93@gmail.com>
2018-11-15 10:32:43 +0000

    modified template files

--- config/deploy/production.example.rb
+++ config/deploy/production.example.rb
@@ -3,10 +3,10 @@
 ################################################################################
 
 # The Git branch this environment should be attached to.
-set :branch, 'development'
+set :branch, 'master'
 
 # The environment's name. To be used in commands and other references.
-set :stage, :staging
+set :stage, :production
 
 # The URL of the website in this environment.
 set :stage_url, 'http://www.example.com'
@@ -16,6 +16,3 @@ server 'XXX.XXX.XX.XXX', user: 'SSHUSER', roles: %w(web app db)
 
 # The deploy path to the website on this environment's server.
 set :deploy_to, '/deploy/to/path'
-
-# The web user on this environment's server.
-set :web_user, 'www-data'
